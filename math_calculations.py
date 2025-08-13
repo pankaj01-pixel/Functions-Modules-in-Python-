15fdab582589130d98836563a6937b34f34ae643
pankaj01-pixel <125261094+pankaj01-pixel@users.noreply.github.com>
2025-08-13 22:06:57 +0530

    Add files via upload

---
+++ math_calculations.py
@@ -0,0 +1,12 @@
+
+import math
+
+User_number = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(User_number)
+log_val = math.log(User_number)
+sin_val = math.sin(User_number)
+
+print(f"Square root  : {sqrt_val}")
+print(f"Natural logarithm : {log_val}")
+print(f"Sine : {sin_val}")
